ee2f3de4134614f39ce4f7f47545864785f35614
aldbas <bascugaldwin@gmail.com>
2019-09-09 19:26:31 -0700

    blocks ontap turn red and drop

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/gestures.dart';
+
+import 'langaw-game.dart';
+
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  LangawGame game = LangawGame();
+  runApp(game.widget);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  flameUtil.addGestureRecognizer(tapper);
+}
